b666ff77b33cb96f7bb022e6863e740a15f157ae
Banti Sheoran <bantisheoran2@gmail.com>
2024-07-15 17:36:14 +0530

    Commit done by Banti for the first time

---
+++ src/test/java/testing/BasicClass.java
@@ -0,0 +1,24 @@
+package testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BasicClass {
+	WebDriver driver;
+	
+	@BeforeTest
+	public void baseMethod() {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		driver=new ChromeDriver();
+	}
+	
+@Test
+public void firstTest() {
+	driver.get("chrome://settings/help");
+	driver.close();
+	
+	
+}
+}
